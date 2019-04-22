e3ebfccd3ec8a62918227d60ed6f7db42ee08c49
Gunjan Datta <me@dattabase.com>
2019-04-22 12:05:25 -0400

    Updated the core lib.

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,5 @@ import { MyInfo } from "./myInfo";
 // Create a global variable for the configuration
 window["DemoRibbon"] = { Configuration };
 
-// Wait for the page to be loaded
-window.addEventListener("load", () => {
-    // Create an instance of the ribbon
-    MyInfo();
-});
+// Create an instance of the ribbon
+MyInfo();
